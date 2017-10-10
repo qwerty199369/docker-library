7467e62b0e861d4c5cf87d912b12a92e7b90206c
qwerty199369 <qwerty199369@outlook.com>
2017-10-10 16:00:29 +0800

    docker install

--- docker_install.sh
+++ docker_install.sh
@@ -1,12 +1,10 @@
+#!/bin/bash
 
-sed -r -i -e "s/archive.ubuntu.com/mirrors.163.com/g" /etc/apt/sources.list
-sed -r -i -e "s/security.ubuntu.com/mirrors.163.com/g" /etc/apt/sources.list
+sed -r -i -e "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
 
 apt-get -y update && apt-get -y upgrade
 
 apt-get remove docker docker-engine docker.io
-
-cd /usr/local/src
 
 apt-get -y install \
     apt-transport-https \
@@ -14,18 +12,17 @@ apt-get -y install \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 apt-key fingerprint 0EBFCD88
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get -y update && apt-get -y upgrade
 
 apt-get -y install docker-ce
+
+cd /usr/local/src
 
 docker run hello-world
 
