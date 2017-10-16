78e137620251a632e5b7026646c756018ca60416
qwerty199369 <qwerty199369@outlook.com>
2017-10-16 18:53:43 +0800

    docker_run.sh

--- nginx-php-mysql-redis/docker_run.sh
+++ nginx-php-mysql-redis/docker_run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-chown -R nginx:appserver /usr/local/nginx
-chown -R php:appserver   /usr/local/php
-chown -R mysql:appserver /usr/local/mysql
-chown -R redis:appserver /usr/local/redis
+chown -R nginx:appserver  /usr/local/nginx
+chown -R php:appserver    /usr/local/php
+chown -R mysql:appserver  /usr/local/mysql
+chown -R redis:appserver  /usr/local/redis
+
+# start mysql
+if [ -z "$(ls -A /usr/local/mysql/datadir)" ]; then
+   /usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --initialize-insecure --user=mysql --basedir=/usr/local/mysql --datadir=/usr/local/mysql/datadir
+   sleep 1s
+fi
+
+/usr/local/mysql/support-files/mysql.server start
+sleep 1s
+chmod 777 /tmp/mysql.sock
 
 # start php-fpm
 /usr/local/php/sbin/php-fpm
